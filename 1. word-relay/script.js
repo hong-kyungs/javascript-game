87100c90d21616212d7d74a3497f45922310fc8f
hong-kyungs <hkyungs11@naver.com>
2022-09-24 16:40:34 +0900

    끝말잇기게임 쿵쿵따로 완성

---
+++ 1. word-relay/script.js
@@ -0,0 +1,45 @@
+const number = parseInt(prompt('몇 명이 참가하나요?'));
+
+if (number) {
+	const input = document.querySelector('input');
+	const button = document.querySelector('button');
+	const givenWord = document.querySelector('#word');
+	const orderNumber = document.querySelector('#order');
+	let word; //제시어
+	let newWord; // 새로 입력한 단어
+
+	const onInput = (event) => {
+		newWord = event.target.value;
+	};
+
+	const onClickButton = () => {
+		//제시어가 비어있는가? 또는 입력한 단어가 올바른가?
+		if (
+			(!word || word[word.length - 1] === newWord[0]) &&
+			newWord.length === 3
+		) {
+			//비어있다
+			word = newWord; //입력한 단어가 제시어가 된다.
+			givenWord.textContent = word;
+			const order = parseInt(orderNumber.textContent); // 현재순서
+			if (order + 1 > number) {
+				orderNumber.textContent = 1;
+			} else {
+				orderNumber.textContent = order + 1;
+			}
+		} else if (
+			(!word || word[word.length - 1] === newWord[0]) &&
+			newWord.length !== 3
+		) {
+			alert('3글자 이상 입력하세요.');
+		} else {
+			// 올바르지 않은가?
+			alert('올바르지 않은 단어입니다.');
+		}
+		input.value = '';
+		input.focus();
+	};
+
+	input.addEventListener('input', onInput);
+	button.addEventListener('click', onClickButton);
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+Javascript로 웹게임 만들기 - by ZeroCho
+
+## 1. 끝말잇기
+
+> 끝말잇기 쿵쿵따게임.  
+> 게임인원수는 게임시작시 설정가능하고, 3글자 단어를 올바르게 넣어야한 통과.
+
+- 게임인원수 설정
+  <img src="image/word-relay1.png" width="300" height="200"/>
+
+- 제시한 단어가 3글자가 아닐경우 에러메세지
+  <img src="image/word-relay2.png" width="300" height="200"/>
+
+- 제시간 단어가 올바르지 않을 경우 에러메세지
+  <img src="image/word-relay3.png" width="300" height="200"/>
+
+- 게임참여자 순번 표시하기
+
+```js
+const order = parseInt(orderNumber.textContent)
+if (order + 1 > number) {
+  orderNumber.textContent = 1
+} else {
+  orderNumber.textContent = order + 1
+}
+```
+
+---
