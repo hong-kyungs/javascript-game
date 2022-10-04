99511d94bf49c05d7843c74d61d5d3a6d1d0eab9
hong-kyungs <hkyungs11@naver.com>
2022-10-04 18:47:06 +0900

    가위바위보 승부결정 - 방법2 & 점수계산

--- 5. rock-scissors-paper/script.js
+++ 5. rock-scissors-paper/script.js
@@ -33,11 +33,24 @@ const changeComputerHand = () => {
 };
 let intervalID = setInterval(changeComputerHand, 50);
 
+// 가위바위보의 규칙찾기
+// 가위: 1, 바위: 0, 보: -1로 생각하고 내가 낸 것과 컴퓨터가 낸 두값의 차이로 공통규칙 찾아내기
+//나\컴퓨터  가위  바위  보
+//가위				0     1    2
+//바위        -1    0    1
+//보          -2    -1   0
+const scoreTable = {
+	rock: 0,
+	scissors: 1,
+	paper: -1,
+};
+
 //버그예제
 //ex) clickbutton 5번 클릭하면(호출), 인터벌 1번, 2번, 3번, 4번, 5번(마지막인 5번만 intervalID에 저장)
 //그 다음에 버튼을 클릭하면 clearInterval로 5번만 취소되고,
 //1~4번은 실행되게 된다. 그래서 클릭할수록 그림이 더 빠르게 돌아간다.
 let clickable = true;
+let score = 0;
 const clickButton = () => {
 	//clickable이라는 변수를 만들어 버튼을 클릭하는 동안에는 false로 만든다.
 	//이때 if문의 조건식이 false가 되므로 버튼을 클릭해도 코드가 실행되지 않는다.
@@ -51,6 +64,23 @@ const clickButton = () => {
 				: event.target.textContent === '가위'
 				? 'scissors'
 				: 'paper';
+		const myScore = scoreTable[myChoice];
+		const computerScore = scoreTable[computerChoice];
+		const diff = myScore - computerScore;
+		console.log(computerChoice, myChoice, computerScore, myScore, diff);
+		let message;
+		if (diff === -1 || diff === 2) {
+			score += 1;
+			message = '승리';
+		} else if (diff === 1 || diff === -2) {
+			score -= 1;
+			message = '패배';
+		} else {
+			message = '무승부';
+		}
+		$score.textContent = `${message} 총: ${score}점`;
+
+		/*
 		if (myChoice === 'rock') {
 			if (computerChoice === 'rock') {
 				console.log('무승부');
@@ -76,6 +106,7 @@ const clickButton = () => {
 				console.log('패배');
 			}
 		}
+		*/
 		//점수 계산 및 화면 표시
 		setTimeout(() => {
 			clickable = true;
