7287009dddd8b45c54970d25086e82da4c91448d
hong-kyungs <hkyungs11@naver.com>
2022-10-09 01:39:47 +0900

    시간 차이 저장하기

--- 6. response-check/script.js
+++ 6. response-check/script.js
@@ -1,6 +1,9 @@
 const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
 
+let startTime;
+let endTime;
+const records = [];
 $screen.addEventListener('click', (event) => {
 	if (event.target.classList.contains('waiting')) {
 		//대기화면, 파랑
@@ -13,6 +16,7 @@ $screen.addEventListener('click', (event) => {
 			event.target.classList.add('now');
 			$screen.textContent = '클릭하세요';
 			//첫시간재기
+			startTime = new Date();
 		}, random);
 	} else if (event.target.classList.contains('ready')) {
 		//준비화면, 빨강
@@ -21,5 +25,16 @@ $screen.addEventListener('click', (event) => {
 		//클릭화면, 초록
 		//끝 시간 재기
 		//시간 차이 저장하기
+		endTime = new Date();
+		const current = endTime - startTime;
+		records.push(current);
+		//평균값 구할때 reduce
+		const average = records.reduce((a, c) => a + c) / records.length;
+		$result.textContent = `현재 ${current}ms, 평균 ${average}ms`;
+		startTime = null; // 안써도 되지만 만약을 대비해서 초기화시켜줌
+		endTime = null; // 안써도 되지만 만약을 대비해서 초기화시켜줌
+		event.target.classList.remove('now');
+		event.target.classList.add('waiting');
+		$screen.textContent = '클릭해서 시작하세요';
 	}
 });
