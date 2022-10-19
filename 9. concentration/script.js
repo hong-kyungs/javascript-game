24cd90ea2b29a0bdbe080198748f397016b68ea1
hong-kyungs <hkyungs11@naver.com>
2022-10-20 01:57:03 +0900

    카드 짝 맞추기

--- 9. concentration/script.js
+++ 9. concentration/script.js
@@ -11,6 +11,8 @@ const colors = [
 ];
 let colorCopy = colors.concat(colors);
 let shuffled = [];
+let clicked = [];
+let completed = [];
 
 function shuffle() {
 	for (let i = 0; colorCopy.length > 0; i += 1) {
@@ -39,10 +41,46 @@ function createCard(i) {
 	return card;
 }
 
+function onClickCard() {
+	this.classList.toggle('flipped');
+	clicked.push(this);
+	//if문의 중첩을 방지하지 위해서 return을 빨리한다.
+	if (clicked.length !== 2) {
+		return;
+	}
+	//클릭한 카드가 두개일때만 실행
+	const firstBackColor =
+		clicked[0].querySelector('.card-back').style.backgroundColor;
+	const secondBackColor =
+		clicked[1].querySelector('.card-back').style.backgroundColor;
+	if (firstBackColor === secondBackColor) {
+		// 두 카드가 같으면
+		completed.push(clicked[0]);
+		completed.push(clicked[1]);
+		clicked = [];
+		//위 세줄을 줄여서 아래로 만들어줄 수 있다.
+		//completed = completed.concat(clicked);
+		if (completed.length !== total) {
+			return;
+		}
+		setTimeout(() => {
+			alert('축하합니다!!');
+		}, 1000);
+		return;
+	}
+	//두 카드가 다르면
+	setTimeout(() => {
+		clicked[0].classList.remove('flipped');
+		clicked[1].classList.remove('flipped');
+		clicked = [];
+	}, 500);
+}
+
 function startGame() {
 	shuffle();
 	for (let i = 0; i < total; i += 1) {
 		const card = createCard(i);
+		card.addEventListener('click', onClickCard);
 		$wrapper.appendChild(card);
 	}
 
