47ace9507ea11b56f76d87a59dcbfd71f33ebcd4
hong-kyungs <hkyungs11@naver.com>
2022-10-28 23:51:35 +0900

    지뢰심기

--- 10. mind-sweeper/script.js
+++ 10. mind-sweeper/script.js
@@ -1,4 +1,4 @@
-$tBody = document.querySelector('table tbody');
+$tbody = document.querySelector('table tbody');
 $result = document.querySelector('#result');
 const row = 10; //줄
 const cell = 10; //칸
@@ -13,10 +13,57 @@ const CODE = {
 	OPENED: 0, // 0 이상이면 모두 열린 칸
 };
 
-function plantMine() {}
+let data;
+function plantMine() {
+	const candidate = Array(row * cell)
+		.fill()
+		.map((arr, i) => {
+			return i;
+		});
+	const shuffle = [];
+	while (candidate.length > row * cell - mine) {
+		const chosen = candidate.splice(
+			Math.floor(Math.random() * candidate.length),
+			1
+		)[0];
+		shuffle.push(chosen);
+	}
+	data = [];
+	for (let i = 0; i < row; i++) {
+		const rowData = [];
+		data.push(rowData);
+		for (let j = 0; j < cell; j++) {
+			rowData.push(CODE.NORMAL);
+		}
+	}
+
+	/*
+	shuffle = [85, 19, 93]를 예를 들면 85는 8번째 줄, 5번째칸
+	for (let k = 0; k < shuffle.length; k++) {
+		const ver = Math.floor(85 / 10); // 8번째 줄
+		const hor = 85 % 10; // 5번째 칸
+	}
+	*/
+	for (let k = 0; k < shuffle.length; k++) {
+		const ver = Math.floor(shuffle[k] / cell); // 몇번째 줄
+		const hor = shuffle[k] % cell; // 몇번째 칸
+		data[ver][hor] = CODE.MINE;
+	}
+}
 
 function drawTable() {
 	plantMine();
+	data.forEach((row) => {
+		const $tr = document.createElement('tr');
+		row.forEach((cell) => {
+			const $td = document.createElement('td');
+			if (cell === CODE.MINE) {
+				$td.textContent = 'X'; // 개발 편의를 위해 일단 지뢰위치 표시, 개발자모드
+			}
+			$tr.append($td);
+		});
+		$tbody.append($tr);
+	});
 }
 
 drawTable();
