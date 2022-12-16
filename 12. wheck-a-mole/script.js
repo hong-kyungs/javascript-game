f951f64fea28dda8a582e46c214f835819211eaa
hong-kyungs <hkyungs11@naver.com>
2022-12-16 15:46:42 +0900

    두더지30%, 폭탄20%, 빈칸50%로 나오게하기

--- 12. wheck-a-mole/script.js
+++ 12. wheck-a-mole/script.js
@@ -15,17 +15,29 @@ $start.addEventListener('click', () => {
 	tick();
 });
 
+let gopherPercent = 0.3;
+let bombPercent = 0.5;
 function tick() {
 	holes.forEach((hole, index) => {
 		//holes 배열안에 값이 있으면 두더지가 보이는 상황이므로 이때는 타이머를 추가하지 않는다
-		//무언가 일어나고 있으면 return, 이 코드로 인해서 setTimeout이 2초에 한번 실행됨
-		if (hole) return;
-		const $gopher = $$cells[index].querySelector('.gopher');
-		holes[index] = setTimeout(() => {
-			// 1초 뒤에 사라짐
-			$gopher.classList.add('hidden');
-			holes[index] = 0;
-		}, 1000);
-		$gopher.classList.remove('hidden');
+		if (hole) return; //무언가 일어나고 있으면 return, 이 코드로 인해서 setTimeout이 2초에 한번 실행됨
+		const randomValue = Math.random();
+		if (randomValue < gopherPercent) {
+			const $gopher = $$cells[index].querySelector('.gopher');
+			holes[index] = setTimeout(() => {
+				// 1초 뒤에 사라짐
+				$gopher.classList.add('hidden');
+				holes[index] = 0;
+			}, 1000);
+			$gopher.classList.remove('hidden');
+		} else if (randomValue < bombPercent) {
+			const $bombs = $$cells[index].querySelector('.bomb');
+			holes[index] = setTimeout(() => {
+				// 1초 뒤에 사라짐
+				$bombs.classList.add('hidden');
+				holes[index] = 0;
+			}, 1000);
+			$bombs.classList.remove('hidden');
+		}
 	});
 }
